983c97e99aae85e160e31cca6e3c9796933bcc7d
tomasdiazkelly <tomasdiazkelly@outlook.com>
2023-11-06 00:34:08 -0300

    ac

--- src/cartManager.js
+++ src/cartManager.js
@@ -16,7 +16,6 @@ class CartManager {
         } catch (error) {
             console.error('Error al escribir en el archivo:', error);
         }
-
         return this.carts[cartId];
     }
 
@@ -26,27 +25,31 @@ class CartManager {
 
     async addProductToCart(cartId, productId, quantity) {
         const cart = this.carts[cartId];
-        const existingProduct = cart.products.find(product => product.id === productId);
-
-        if (existingProduct) {
-            existingProduct.quantity += quantity;
+    
+        if (cart) {
+            const existingProduct = cart.products.find(product => product.id === productId);
+    
+            if (existingProduct) {
+                existingProduct.quantity += quantity;
+            } else {
+                cart.products.push({ id: productId, quantity });
+            }
+    
+            try {
+                await this.writeCartsToFile();
+            } catch (error) {
+                console.error('Error al escribir en el archivo:', error);
+            }
+            return cart;
         } else {
-            cart.products.push({ id: productId, quantity });
+            return null;
         }
-
-        try {
-            await this.writeCartsToFile();
-        } catch (error) {
-            console.error('Error al escribir en el archivo:', error);
-        }
-
-        return cart;
     }
-
     async writeCartsToFile() {
         try {
-            await fs.writeFile('cart.json', JSON.stringify(this.carts, null, 2));
+            await fs.writeFile('src/cart.json', JSON.stringify(this.carts, null, 2));
             console.log('Carts escritos en el archivo correctamente.');
+            
         } catch (error) {
             throw new Error('Error al escribir en el archivo: ' + error.message);
         }
